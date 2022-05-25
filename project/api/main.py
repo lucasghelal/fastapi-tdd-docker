62a3ef7a8a7c109c22b22705a26e484cc6599e0d
Lucas Helal <lucasghelal@gmail.com>
2022-05-25 08:35:50 -0300

    configured tortoise orm

--- project/api/main.py
+++ project/api/main.py
@@ -1,4 +1,7 @@
+import os
+
 from fastapi import Depends, FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 from api.config import get_settings, Settings
 
@@ -6,6 +9,15 @@ from api.config import get_settings, Settings
 app = FastAPI(
     version='0.0.2'
 )
+
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["api.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
+
 
 @app.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
---
+++ project/api/models/tortoise.py
@@ -0,0 +1,14 @@
+from tortoise import fields, models
+
+
+class TextSummary(models.Model):
+    """
+    TextSummary model
+    """
+
+    url = fields.TextField()
+    summary = fields.TextField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.url
