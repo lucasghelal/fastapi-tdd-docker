d8361927955eab0ff3f61d8a3b6614d12a0490e5
Lucas Helal <lucasghelal@gmail.com>
2022-05-25 09:32:49 -0300

    clean up main and add logs

--- project/api/main.py
+++ project/api/main.py
@@ -1,25 +1,27 @@
-import os
+import logging
 
 from fastapi import FastAPI
-from tortoise.contrib.fastapi import register_tortoise
 
 from api.router import ping
+from api.db import init_db
 
+
+log = logging.getLogger("uvicorn")
 
 def create_application() -> FastAPI:
-    application = FastAPI(version='0.0.3')
-
-    register_tortoise(
-        application,
-        db_url=os.environ.get("DATABASE_URL"),
-        modules={"models": ["api.models.tortoise"]},
-        generate_schemas=False,
-        add_exception_handlers=True,
-    )
-    
+    application = FastAPI(version='0.0.3')        
     application.include_router(ping.router)
 
     return application
 
-
 app = create_application()
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
